62ee403d8c01d67ea660c37130f6c0e69678ba3a
Nkzn <nkzn@nkzn.net>
2012-06-12 11:36:37 +0900

    初回コミット

---
+++ src/main/java/net/nkzn/jsonpullparser/util/converter/ListAllConverter.java
@@ -0,0 +1,9 @@
+package net.nkzn.jsonpullparser.util.converter;
+
+import java.util.List;
+
+import net.vvakame.util.jsonpullparser.util.TokenConverter;
+
+public class ListAllConverter extends TokenConverter<List<String>> {
+
+}
---
+++ src/test/java/net/nkzn/jsonpullparser/util/converter/ListAllConverterTest.java
@@ -0,0 +1,14 @@
+package net.nkzn.jsonpullparser.util.converter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ListAllConverterTest {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+	}
+
+}
